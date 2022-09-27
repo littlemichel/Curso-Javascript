004338e6053b5c8e2911cee9ec859e468f38e304
littlemichel <80757141+littlemichel@users.noreply.github.com>
2022-09-27 22:49:06 +0100

    main

---
+++ app.js
@@ -0,0 +1,26 @@
+// alert('Hola desde app.js');
+
+// console.log( console.log('Hola Mundo') );
+
+let a = 10,
+    b = 20,
+    c = "Hola ",
+    d = "Spiderman",
+    x = a + b;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//const saludo = c + d;
+//console.log(saludo);
+
+// console.log('%c Mis variables', 'color:blue; font-weight: bold');
+// console.log({ a }); //objeto
+// console.log({ b }); //objeto
+// console.log({ c }); //objeto
+
+//console.table(a, b, c, d, x ); //lista
+//console.table({a, b, c, d, x }); //objetos
+
+
