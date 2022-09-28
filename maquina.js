a0da5da9abb511272322e175bdb27bfc32315ed1
Alma Olave <michelle.debastos@gmail.com>
2022-09-28 15:47:50 +0100

    main

--- maquina.js
+++ maquina.js
@@ -5,6 +5,18 @@ productos = { name : "Doritos", quantity : 2, price: 1.90 },
             { name : "Cocacola", quantity : 4 , price : 1.00},
             { name : "Twinx", quantity: 5, price : 0.80 };
 
+
+//paqueteria para leer consola (Detiene los procesos del programa)
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+readline.question("Seleccione ")
+
+
+
 var readline = require('readline');
 
 var rl = readline.createInterface({
