e0902962eebd36c51c7f5795f4764f92b8d49b9a
Alma Olave <michelle.debastos@gmail.com>
2022-09-28 16:44:30 +0100

    main

--- mensajes.js
+++ mensajes.js
@@ -1,13 +1,33 @@
 require('colors');
 
-const mostrarMenu = () => {
+function mostrarMenu() {
 
     console.clear();
     console.log('----------------------------------'.green);
     console.log('      Selecione una opción'.green);
     console.log('----------------------------------\n'.green);
 
-    console.log(`1. Crear Tarea`);
-    
+    console.log(`${'1.'.green} Crear Tarea`);
+    console.log(`${'2.'.green} Listar Tareas`);
+    console.log(`${'3.'.green} Listar Tareas Completas`);
+    console.log(`${'4.'.green} Listar Tareas pendientes`);
+    console.log(`${'5.'.green} Completar tarea(s)`);
+    console.log(`${'6.'.green} Borrar tarea`);
+    console.log(`${'0.'.green} Salir \n`);
 
+    const readline = require("readline").createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readline.question('Seleccione una opción: ', (opt) => {
+        console.log({ opt });
+        readline.close();
+    });
+
+}
+
+
+module.exports = {
+    mostrarMenu
 }
