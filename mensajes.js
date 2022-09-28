dc0c1bd817b3dae7d31e6bc0d4b635f0ca3dba12
Alma Olave <michelle.debastos@gmail.com>
2022-09-28 16:11:00 +0100

    main

---
+++ mensajes.js
@@ -0,0 +1,13 @@
+require('colors');
+
+const mostrarMenu = () => {
+
+    console.clear();
+    console.log('----------------------------------'.green);
+    console.log('      Selecione una opción'.green);
+    console.log('----------------------------------\n'.green);
+
+    console.log(`1. Crear Tarea`);
+    
+
+}
