2656ff2fe809b08fa37bd4314bf9dfa06de9146d
Alma Olave <michelle.debastos@gmail.com>
2022-10-03 14:18:28 +0100

    main

---
+++ 02-fundamentos/flecha.js
@@ -0,0 +1,17 @@
+//Funcion antigua (solo utilizar cuando sea estrictamente nesecario)
+//function sumar (a,b){
+//    return a + b;
+//}
+
+//Funcion larga
+//const sumar = (a, b) => {
+//    return a + b;
+//};
+
+const sumar = (a, b) => a + b;
+const saludar = ( ) => 'Hola Mundo';
+
+
+console.log(sumar(5));
+console.log(sumar(5,10));
+console.log( saludar() );
