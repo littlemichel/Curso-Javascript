b8dae45a067a4460c23ca180ac6d64950b67c66d
Alma Olave <michelle.debastos@gmail.com>
2022-10-07 13:11:08 +0100

    teste

---
+++ 02-fundamentos/callback-hell.js
@@ -0,0 +1,16 @@
+const empleados = [
+    {
+        id: 1,
+        nombre: "Pedro"
+    },
+    {
+        id: 2,
+        nombre: "Joaquim"
+    },
+    {
+        id: 3,
+        nombre: "El Pepe"
+    }
+];
+
+const getEmpleado
