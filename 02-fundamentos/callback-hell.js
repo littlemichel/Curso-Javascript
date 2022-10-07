a6469c9d8104d595598f4e67b8948b7548813bc9
Alma Olave <michelle.debastos@gmail.com>
2022-10-07 14:04:00 +0100

    0.1

--- 02-fundamentos/callback-hell.js
+++ 02-fundamentos/callback-hell.js
@@ -13,4 +13,53 @@ const empleados = [
     }
 ];
 
-const getEmpleado
+
+
+const Salarios = [
+    {
+        id:1,
+        salario: 500
+    },
+    {
+        id:2,
+        salario:1000
+    },
+    {
+        id:3,
+        salario:1500
+    }
+];
+
+const getSalario = ( id ) => {
+    const salario = salarios.find( (e) => { 
+        return e.id === id
+    })
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const getEmpleado = (id) => {
+    const empleado = empleados.find( (e) =>{
+        return e.id === id
+    })
+}
