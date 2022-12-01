b5dab7b98c5206706937ccc763f89bc0015a3516
Alma Olave <michelle.debastos@gmail.com>
2022-12-01 18:24:57 +0000

    funcion for

---
+++ 03-bases-node/app.js
@@ -0,0 +1,9 @@
+// imprimir la tabla del 5
+//i++ va sumando un numero al valor
+// Esta funciona tiene dentro la misma contruccion de variable
+
+for (let i = 0; i <= 10; i++){
+    console.log(`5 x ${i} = ${i * 5}`)
+};
+
+     
